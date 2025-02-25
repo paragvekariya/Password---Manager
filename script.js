7024e7cafa55c65aace2fe8baaec8add2c6d3094
vekariyaparag <vekariyaparag07@gmail.com>
2025-02-25 20:22:12 +0530

    work on script

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("work");
+document.querySelector(".btn").addEventListener("click",  (e)=>{
+    e.preventDefault()
+    console.log("cliked please..")
+    console.log(username.value, password.value)
+}
+)
